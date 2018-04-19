2aed9d7a968bb56396715b4721102d13d9c05682
ayeshadias <32886121+ayeshadias@users.noreply.github.com>
2018-04-19 15:41:55 +1000

    upload

---
+++ 5_archive/assets/js/main.js
@@ -0,0 +1,173 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+$('.b-reset').click(function(){
+
+  $('.item').addClass('show');
+  $('.item').removeClass('hide');
+
+
+});
+
+$('.b-architecture').click(function(){
+
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.architecture').addClass('show');
+  $('.item.architecture').removeClass('hide');
+
+});
+
+
+$('.b-design').click(function(){
+
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.design').addClass('show');
+  $('.item.design').removeClass('hide');
+
+});
+
+
+$('.b-steps').click(function(){
+
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.steps').addClass('show');
+  $('.item.steps').removeClass('hide');
+
+});
+
+
+$('.b-pattern').click(function(){
+
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.pattern').addClass('show');
+  $('.item.pattern').removeClass('hide');
+
+});
+
+
+$('.b-geometry').click(function(){
+
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.geometry').addClass('show');
+  $('.item.geometry').removeClass('hide');
+
+});
+
+
+$('.b-drawing').click(function(){
+
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.drawing').addClass('show');
+  $('.item.drawing').removeClass('hide');
+
+});
+
+
+$('.b-paint').click(function(){
+
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.paint').addClass('show');
+  $('.item.paint').removeClass('hide');
+
+});
+
+
+$('.b-photo').click(function(){
+
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.photo').addClass('show');
+  $('.item.photo').removeClass('hide');
+
+});
+
+
+$('.b-tattoo').click(function(){
+
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.tattoo').addClass('show');
+  $('.item.tattoo').removeClass('hide');
+
+});
+
+
+$('.b-sculpt').click(function(){
+
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.sculpt').addClass('show');
+  $('.item.sculpt').removeClass('hide');
+
+});
+
+
+$('.b-nature').click(function(){
+
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.nature').addClass('show');
+  $('.item.nature').removeClass('hide');
+
+});
+
+
+$('.b-shells').click(function(){
+
+    $('.item').addClass('hide');
+    $('.item').removeClass('show');
+
+    $('.item.shells').addClass('show');
+    $('.item.shells').removeClass('hide');
+
+});
+
+
+$('.b-galaxy').click(function(){
+
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.galaxy').addClass('show');
+  $('.item.galaxy').removeClass('hide');
+
+});
+
+
+$('.b-green').click(function(){
+
+  $('.item').addClass('hide');
+  $('.item').removeClass('show');
+
+  $('.item.green').addClass('show');
+  $('.item.green').removeClass('hide');
+
+});
+
+
+  $('.sample').addClass('classname');
+  $('.sample').removeClass('classname');
+
+
+
+});
