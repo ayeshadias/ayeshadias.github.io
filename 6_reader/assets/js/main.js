e0a346aa1bf78f763ec79665a154f2fb43ac919f
ayeshadias <32886121+ayeshadias@users.noreply.github.com>
2018-06-01 00:42:14 +1000

    upload8929284724
    dckjnksd

--- 6_reader/assets/js/main.js
+++ 6_reader/assets/js/main.js
@@ -9,9 +9,40 @@ $(document).ready(function() {
      $('.p-disp').removeClass('p-show');
    });
 
+
+//////////////////////////
+
+  $('.click1').click(function(){
+    $('.show1').show();
+  });
+
+  $('.click2').click(function(){
+    $('.show2').show();
+  });
+
+  $('.click3').click(function(){
+    $('.show3').show();
+  });
+
+  $('.click4').click(function(){
+    $('.show4').show();
+  });
+
+  $('.click5').click(function(){
+    $('.show5').show();
+  });
+
+  $('.click6').click(function(){
+    $('.show6').show();
+  });
+
+  $('.click7').click(function(){
+    $('.show7').show();
+  });
+
+
+
   $('.sample').addClass('classname');
   $('.sample').removeClass('classname');
-
-
 
 });
